a7c3649d9ff83fceb7ee70206500e02b926730ac
gayathriilavarasan <gayathriilavarasan1@gmail.com>
2023-04-25 10:55:25 +0530

    hospital

--- hospital/src/main/java/com/example/hospital/HospService/HospService.java
+++ hospital/src/main/java/com/example/hospital/HospService/HospService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+
 import com.example.hospital.HospModel.HospModel;
 import com.example.hospital.HospRepo.HospRepo;
 @Service
